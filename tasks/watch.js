fb24bafbdfcedba3de6cf5bdff1cef3a197658a0
Andrey Mishchenko <msav@msav.ru>
2019-02-14 01:11:12 +0300

    Components watch

--- tasks/watch.js
+++ tasks/watch.js
@@ -5,5 +5,20 @@ module.exports = () => {
     mbx.gulp.task("watch", () => {
         mbx.gulp.watch(filterPath(mbx.path.join(mbx.config.srcPath, "sass", "/**/*.scss")), mbx.gulp.series("theme-sass"));
         mbx.gulp.watch(filterPath(mbx.path.join(mbx.config.srcPath, "js", "/**/*.js")), mbx.gulp.series("theme-js"));
+
+        mbx.gulp.watch(
+            [
+                filterPath(mbx.path.join(mbx.config.componentsPath, "/**/src/*.scss")),
+                filterPath(mbx.path.join(mbx.config.componentsPath, "/**/.default/src/*.scss"))
+            ],
+            mbx.gulp.series("components-sass")
+        );
+        mbx.gulp.watch(
+            [
+                filterPath(mbx.path.join(mbx.config.componentsPath, "/**/src/*.js")),
+                filterPath(mbx.path.join(mbx.config.componentsPath, "/**/.default/src/*.js"))
+            ],
+            mbx.gulp.series("components-js")
+        );
     });
 };
