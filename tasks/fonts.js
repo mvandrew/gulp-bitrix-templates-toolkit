1766ada896ff82ee05d9335d156e90eca5da5bea
Andrey Mishchenko <msav@msav.ru>
2019-02-13 13:10:12 +0300

    Added fonts task

---
+++ tasks/fonts.js
@@ -0,0 +1,13 @@
+
+module.exports = () => {
+    mbx.gulp.task("fonts", () => {
+        return mbx.gulp.src(mbx.config.fontFiles)
+            .pipe( mbx.plumber({ errorHandler: function(err) {
+                    mbx.notify.onError({
+                        title: "Gulp error in " + err.plugin,
+                        message:  err.toString()
+                    })(err);
+                }}) )
+            .pipe( mbx.gulp.dest(mbx.path.join(mbx.config.assetsPath, "fonts")) );
+    });
+};
--- tasks/index.js
+++ tasks/index.js
@@ -1,5 +1,6 @@
 module.exports = scriptPath => {
     return [
+        mbx.path.join(scriptPath, "tasks", "fonts.js"),
         mbx.path.join(scriptPath, "tasks", "vendor-js.js"),
         mbx.path.join(scriptPath, "tasks", "vendor-css.js"),
         mbx.path.join(scriptPath, "tasks", "theme-js.js"),
