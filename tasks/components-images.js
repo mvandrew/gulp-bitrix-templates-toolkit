d53e299ba20568d37375578115a94ec110ba8c34
Andrey Mishchenko <msav@msav.ru>
2019-03-24 17:11:48 +0300

    Copy components gif

--- tasks/components-images.js
+++ tasks/components-images.js
@@ -3,8 +3,8 @@ const libImages = require("../lib/components-images");
 module.exports = () => {
     mbx.gulp.task("components-images", () => {
         const src   = [
-            mbx.path.join(mbx.config.componentsPath, "/**/src/images/**/*.+(jpeg|jpg|png)"),
-            mbx.path.join(mbx.config.componentsPath, "/**/.default/src/images/**/*.+(jpeg|jpg|png)")
+            mbx.path.join(mbx.config.componentsPath, "/**/src/images/**/*.+(jpeg|jpg|png|gif)"),
+            mbx.path.join(mbx.config.componentsPath, "/**/.default/src/images/**/*.+(jpeg|jpg|png|gif)")
         ];
         const dest = mbx.config.componentsPath;
         return libImages(src, dest);
